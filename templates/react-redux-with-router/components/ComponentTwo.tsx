3441f7191317780b339297e84062921f8b1b7107
Karan Rajpal <karanrajpal001@gmail.com>
2020-03-29 22:24:43 -0400

    Styling for both components

--- templates/react-redux-with-router/components/ComponentTwo.tsx
+++ templates/react-redux-with-router/components/ComponentTwo.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 
 export const ComponentTwo = () => (
     <div className='component-two'>
         <h1>Welcome to the second page of your app</h1>
     </div>
 );
-
